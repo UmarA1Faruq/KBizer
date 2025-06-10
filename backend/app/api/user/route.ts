ec3ca0f3c6434474ca3fda809d9ddf64d92a6b14
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-06-10 20:13:54 +0700

    umar: membuat struktur route dan service GET Data User

---
+++ backend/app/api/user/route.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+// buat variabel prisma
+const prisma = new PrismaClient
+
+// buat fungsi untuk service "GET"
+export const GET = async (respon: NextRequest) => {
+    // proses "GET" (tampil data) tb_user
+    const data = await prisma.tb_user.findMany({})
+
+    //Kondisi jika data tidak ditemukan
+    if(data.length == 0)
+    {
+        return NextResponse.json({
+            meta_data:{
+                error: 1,
+                message: "Data User Tidak Ditemukan",
+                status: 404
+            },
+        },{
+            status: 404
+        })
+    }
+    // tampilkan hasil respon
+    return NextResponse.json({
+        meta_data:{
+            error: 0,
+            message: null,
+            status: 200
+        },
+        data_user: data
+    },{
+        status: 200
+    })
+}
+
