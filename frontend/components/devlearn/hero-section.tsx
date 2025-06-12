2af6e797dcc65a10ec72b695c7b03f66c6ecfe3b
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-06-12 17:39:05 +0700

    nurhidayat: membuat struktur dasar komponen pada hero-section.tsx

---
+++ frontend/components/devlearn/hero-section.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <section>
+      <div>
+        <h1></h1>
+        <p></p>
+      </div>
+    </section>
+  );
+}
