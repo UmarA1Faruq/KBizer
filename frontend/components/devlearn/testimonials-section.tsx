1445bac4f4548731109611f02e5e25b27fbd4e87
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-06-14 18:47:47 +0700

    nurhidayat: Membuat struktur awal komponen TestimonialsSection

---
+++ frontend/components/devlearn/testimonials-section.tsx
@@ -0,0 +1,9 @@
+export default function TestimonialsSection() {
+  return (
+    <section id="testimonials" className="py-16 md:py-24 bg-secondary">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Content will be added here */}
+      </div>
+    </section>
+  );
+}
