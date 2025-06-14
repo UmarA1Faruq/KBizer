62bcf0c2a94042ef8a011649d293dcc4997f8173
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-06-14 18:01:42 +0700

    nurhidayat: membuat struktur awal komponen AboutSection

---
+++ frontend/components/devlearn/about-section.tsx
@@ -0,0 +1,12 @@
+// components/AboutSection.tsx
+import React from "react";
+
+export default function AboutSection() {
+  return (
+    <section id="about" className="py-16 md:py-24 bg-secondary">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Content will be added here */}
+      </div>
+    </section>
+  );
+}
