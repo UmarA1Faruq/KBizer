698095ad4575b679de2d39c4860f9c66af640158
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-06-14 14:16:50 +0700

    umar: membuat service GET detail data USER

--- backend/app/api/user/[id]/route.ts
+++ backend/app/api/user/[id]/route.ts
@@ -62,3 +62,54 @@ export const DELETE = async (request: NextRequest, props: { params: Promise<{ id
         })
     }
 }
+
+// buat service "GET" (detail data) tb_user
+export const GET = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
+    const params = await props.params;
+    try {
+        // cek apakah "id" tersedia/tidak
+        const checkId = await prisma.tb_user.findUnique({
+            where: {
+                id: Number(params.id),
+            }
+        })
+
+        //Kondisi jika data tidak ditemukan
+        if (!checkId) {
+            return NextResponse.json({
+                meta_data: {
+                    error: 1,
+                    message: "Data User Tidak Ditemukan",
+                    status: 404
+                },
+            }, {
+                status: 404
+            })
+        }
+
+        // tampilkan hasil respon
+        return NextResponse.json({
+            meta_data: {
+                error: 0,
+                message: null,
+                status: 200
+            },
+            data_user: checkId
+        }, {
+            status: 200
+        })
+    }
+    catch (error: any) {
+        // tampilkan hasil respon
+        return NextResponse.json({
+            meta_data: {
+                error: 1,
+                message: "Parameter Slug (ID) Harus Angka!",
+                status: 400
+            },
+
+        }, {
+            status: 400
+        })
+    }
+}
