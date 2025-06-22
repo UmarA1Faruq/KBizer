78622ec453cd4cc85611c20f27183ad2bfefb11b
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-06-22 11:32:39 +0700

    umar: menambahkan error handling dengan try-catch untuk menangkap kesalahan parameter

--- backend/app/api/course/[id]/route.ts
+++ backend/app/api/course/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,5 +8,19 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export const DELETE = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
-  const params = await props.params;
-};
+    const params = await props.params;
+    try {
+        // logic
+    } catch (error: any) {
+        return NextResponse.json({
+            meta_data: {
+                error: 1,
+                message: "Parameter Slug (ID) Harus Angka!",
+                status: 400
+            }
+        }, {
+            status: 400
+        })
+    }
+
+}
