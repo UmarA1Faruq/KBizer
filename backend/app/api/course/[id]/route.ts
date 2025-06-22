b08e66e72835dcf89e711d21c9106610176563dc
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-06-22 11:26:47 +0700

    umar: membuat struktur dasar fungsi async DELETE course

--- backend/app/api/course/[id]/route.ts
+++ backend/app/api/course/[id]/route.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 
 // buat variabel prisma
 const prisma = new PrismaClient();
+
+export const DELETE = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
+  
+};
